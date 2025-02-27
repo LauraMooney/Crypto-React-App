9f198bc851d73a54373ba300de94292df6f83b1c
Laura Mooney <lauramooney@Lauras-MacBook-Pro.local>
2025-02-27 16:30:07 +0000

    updated variables for coinranking

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,3 +1,4 @@
+/* eslint-disable implicit-arrow-linebreak */
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // Note: Change v1 to v2 on rapid api
@@ -15,17 +16,13 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
-
     getCryptoDetails: builder.query({
       query: (coinId) => createRequest(`/coin/${coinId}`),
     }),
-
-    // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
     getCryptoHistory: builder.query({
-      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+      query: ({ coinId, timeperiod }) =>
+        createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
     }),
-
-    // Note: To access this endpoint you need premium plan
     getExchanges: builder.query({
       query: () => createRequest('/exchanges'),
     }),
